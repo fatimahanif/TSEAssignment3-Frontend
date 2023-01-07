c545b94f2e4891053fab67201379a72043e70bb4
fatimahanif <fatimahanif303@gmail.com>
2023-01-07 12:37:15 +0500

    Created basic layout files

---
+++ src/components/Layout.js
@@ -0,0 +1,23 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
