98488cdaba94fb901e873fc62e4743363477893a
fatimahanif <fatimahanif303@gmail.com>
2023-01-08 20:32:06 +0500

    home and contact

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,20 @@
 const About = () => {
+  const happy = `:)`;
   return (
-    <>
-      <p>About page</p>
-    </>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: "2% 15%",
+        alignItems: "center",
+        alignSelf: "center",
+      }}
+    >
+      <img width="35%" src="images/developer.png" alt="under development" />
+      <p style={{ fontSize: 36, fontWeight: "bold", paddingTop: "2%" }}>
+        This page is still under development! {happy}
+      </p>
+    </div>
   );
 };
 
